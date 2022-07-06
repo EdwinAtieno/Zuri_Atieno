a4db5f6a828a478754ed5563170fc8d8a9e6ea77
eddym <edwinmuthee11@gmail.com>
2022-07-06 11:26:19 +0300

    setting updates

--- Atieno/settings.py
+++ Atieno/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'eddy.apps.EddyConfig',
     'django.contrib.admin',
     'django.contrib.auth',
+    '',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',

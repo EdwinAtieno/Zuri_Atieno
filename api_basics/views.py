a16eadafb1adee027d9366564a67e57596b106c7
eddym <edwinmuthee11@gmail.com>
2022-07-20 13:46:30 +0300

    Update and Deletion Methods

--- api_basics/views.py
+++ api_basics/views.py
@@ -27,3 +27,22 @@ def article_list(request):
 
 @csrf_exempt
 def article_details(request, pk):
+    try:
+        article = Article.objects.get(pk=pk)
+    except Article.DoesNotExist:
+        return HttpResponse(status=404)
+    if request.method == "GET":
+        serializer = ArticleSerializer(article)
+        return JsonResponse(serializer.data)
+    elif request.method == "PUT":
+        data = JSONParser().parse(request)
+        serializer = ArticleSerializer(article, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status= 400)
+
+    elif request.method == "DELETE":
+        article.delete()
+        return HttpResponse(status= 204)
+
